0073813f34dea6e1a2f8a47ac23140637ff7edc2
Stina Persson <stpe1901@student.miun.se>
2025-02-05 15:16:27 +0100

    POST-anrop fungerar som det ska + validering fixad

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -6,10 +6,10 @@ function TodoForm() {
     //interface för fomulär
     
     interface FormInterface {
-        title: string, 
-        description: string, 
-        priority: number, 
-        status: string
+        todo_title: string, 
+        todo_description: string, 
+        todo_priority: number, 
+        todo_status: string
     };
 
     //interface för errors
@@ -20,52 +20,88 @@ function TodoForm() {
     }
 
     // form states
-    const [formData, setFormData] = useState<FormInterface>({title: "", description: "", priority: 1, status: "Ej påbörjad"}); 
+    const [formData, setFormData] = useState<FormInterface>({todo_title: "", todo_description: "", todo_priority: 1, todo_status: "Ej påbörjad"}); 
 
     //error states 
-    const [formError, setFormErros] = useState<ErrorInterface>({})
+    const [formError, setFormErrors] = useState<ErrorInterface>({})
 
     const validateForm = ((data: FormInterface) => {
         const validationErrors: ErrorInterface = {}; 
 
         //om inget data skickats med i title
-        if(!data.title) {
+        if(!data.todo_title) {
             validationErrors.title = "Du måste ange en todo"
         }
 
-        if(data.priority > 5) {
+        if(data.todo_priority > 5 || data.todo_priority < 1) {
             validationErrors.priority = "Välj en siffra mellan 1-5"
-        }
+        } 
+
+        return validationErrors; 
 
     })
 
     //så att sidan inte laddas om automatiskt vid submit
-    const submitForm = ((event: any) => {
+    const submitForm = (async (event: any) => {
         event.preventDefault(); 
 
         const validationErrors = validateForm(formData)
+
+        if(Object.keys(validationErrors).length > 0) {
+            setFormErrors(validationErrors); 
+
+            console.log("Innehåller fel!")
+        } else {
+            setFormErrors({}); 
+
+            //gör ett POST anrop till databasen om allt är ok
+            try {
+                const response = await fetch("https://hapitodos.onrender.com/todo", {
+                    method: "POST", 
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formData),
+                });
+
+                if(!response.ok) {
+                    throw new Error ("Kunde inte skicka datan")
+                } 
+
+                location.reload(); //alternativt annan lösning. Läsa in funktion för att hämta data här? 
+            } catch(error) {
+                //konsoll test för utveckling
+                console.log(error); 
+            }
+            
+            
+        }
     })
 
     return(
         <form onSubmit={submitForm}>
             <label htmlFor="title">Todo:</label><br />
-            <input type="text" name="title" id="title" value={formData.title}
-            onChange={(event) => setFormData({...formData, title: event.target.value})}
-            /><br />
-
+            <input type="text" name="title" id="title" value={formData.todo_title}
+            onChange={(event) => setFormData({...formData, todo_title: event.target.value})}
+            />
+            <br />
+            {formError.title && <span>{formError.title}</span>}
+            <br />
             <label htmlFor="description">Beskrivning:</label><br />
-            <input type="text" name="description" id="description" value={formData.description} 
-            onChange={(event) => setFormData({...formData, description: event.target.value})}
+            <input type="text" name="description" id="description" value={formData.todo_description} 
+            onChange={(event) => setFormData({...formData, todo_description: event.target.value})}
             /><br />
 
             <label htmlFor="priority">Prioritet:</label><br />
-            <input type="number" name="priority" id="priority" value={formData.priority}
-            onChange={(event) => setFormData({...formData, priority: +(event.target.value)})} //plus gör on sträng till nummer
-            /> <br />
-
+            <input type="number" name="priority" id="priority" value={formData.todo_priority}
+            onChange={(event) => setFormData({...formData, todo_priority: +(event.target.value)})} //plus gör on sträng till nummer
+            /> 
+            <br />
+            {formError.priority && <span>{formError.priority}</span>}
+            <br />
             <label htmlFor="status">Status:</label><br />
-            <select name="status" id="status" value={formData.status}
-            onChange={(event) => setFormData({...formData, status: event.target.value})} 
+            <select name="status" id="status" value={formData.todo_status}
+            onChange={(event) => setFormData({...formData, todo_status: event.target.value})} 
             >
                 <option value="ej påbörjad">Ej påbörjad</option>
                 <option value="påbörjad">Påbörjad</option>
@@ -73,6 +109,7 @@ function TodoForm() {
             </select><br />
 
             <input type="submit" value="Lägg till" />
+
         </form>
     )
 }
