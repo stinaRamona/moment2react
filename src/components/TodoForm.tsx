5cb86e64ac8cd81ca29bdb1918a072abfa9921a3
Stina Persson <stpe1901@student.miun.se>
2025-02-05 10:31:50 +0100

    bundit state till formuläret

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,32 +1,46 @@
+import { useState } from "react";
+
 
 function TodoForm() {
+
+    
+    interface FormInterface {
+        title: string, 
+        description: string, 
+        priority: number, 
+        status: string
+    };
+
+    const [formData, setFormData] = useState<FormInterface>({title: "", description: "", priority: 1, status: "Ej påbörjad"}); 
+
     return(
-        <>
-        <form method="POST">
-            <label htmlFor="todo_title">Todo:</label><br />
-            <input type="text" /><br />
+        <form>
+            <label htmlFor="title">Todo:</label><br />
+            <input type="text" name="title" id="title" value={formData.title}
+            onChange={(event) => setFormData({...formData, title: event.target.value})}
+            /><br />
 
-            <label htmlFor="todo_description">Beskrivning:</label><br />
-            <input type="text" /><br />
+            <label htmlFor="description">Beskrivning:</label><br />
+            <input type="text" name="description" id="description" value={formData.description} 
+            onChange={(event) => setFormData({...formData, description: event.target.value})}
+            /><br />
 
-            <label htmlFor="todo_status">Status:</label><br />
-            <select name="todo_status" id="todo_status">
+            <label htmlFor="priority">Prioritet:</label><br />
+            <input type="number" name="priority" id="priority" value={formData.priority}
+            onChange={(event) => setFormData({...formData, priority: +(event.target.value)})} //plus gör on sträng till nummer
+            /> <br />
+
+            <label htmlFor="status">Status:</label><br />
+            <select name="status" id="status" value={formData.status}
+            onChange={(event) => setFormData({...formData, status: event.target.value})} 
+            >
                 <option value="ej påbörjad">Ej påbörjad</option>
                 <option value="påbörjad">Påbörjad</option>
                 <option value="avslutad">Avslutad</option>
             </select><br />
 
-            <label htmlFor="todo_priority">Prioritet:</label><br />
-            <select name="todo_priority" id="todo_priotiry">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-            </select><br />
-
             <input type="submit" value="Lägg till" />
-            
         </form>
-        </>
     )
 }
 
